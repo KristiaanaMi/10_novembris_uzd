3a7bdbda9d893e76250de5e3d51fe584228cd4a9
KristiaanaMi <kristiana.mihailova@grobinasskola.lv>
2024-11-27 14:14:21 +0200

    Pirmais uzdevums izdarīts

--- uzd_01_1.py
+++ uzd_01_1.py
@@ -1,3 +1,6 @@
 # Uzdevums: 01-1: Izveidot programmu, kas prasa lietotâjam ievadît skaitli n, tad programma aprçíina n+nn+nnn. 
-Rezultâts tiek parâdîts konsolç.
+#Rezultâts tiek parâdîts konsolç.
 
+n = input("Ievadi skaitli n: ")
+summa = int(n) + int(n * 2) + int(n * 3)
+print(f"{n} + {n*2} + {n*3} = {summa}")
