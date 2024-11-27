2d4ebb8da4f2a977a0fbf84daff58c353b4cc584
KristiaanaMi <kristiana.mihailova@grobinasskola.lv>
2024-11-27 14:39:11 +0200

    2 uzdevums

--- uzd_01_2.py
+++ uzd_01_2.py
@@ -2,4 +2,16 @@
 # tilpums = 3.14 * râdiuss * râdiuss * augstums
 # laukums = 2 * (3.14 * râdiuss * râdiuss) + augstums * (2 * 3.14 * râdiuss)
 
+import math
 
+radius=float(input("Lūdzu ievadi cilindra radiusu:"))
+height=float(input("Lūdzu ievadi cilindra augstumu"))
+
+PI=3.14
+
+volume = PI * radius**2 * height
+
+surface_area = 2 * (PI * radius**2) + (2 * PI * radius * height)
+
+print(f"Cilindra tilpums ir : {volume:.27}")
+print(f"Cilindra laukums ir : {surface_area:.2f}")
